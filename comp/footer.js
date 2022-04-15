f0ad71e1f589847d55c97a2eb33f62bf5cb5a4d5
squiggs <brennan@bell.net>
2022-04-14 20:12:20 -0400

    added layout

---
+++ comp/footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return ( 
+
+        <footer>copyright all rights resever</footer>
+     );
+}
+ 
+export default Footer;
---
+++ comp/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+
+        <nav className="logo">
+            <h1>company list</h1>
+            <ul>
+                <li><a>home</a></li>
+                <li><a>about</a></li>
+                <li><a>contact</a></li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
